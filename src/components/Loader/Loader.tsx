83c8e8a123a7062abf532a7981db96f1cb633377
Uliana Seredenko <uliana1331@gmail.com>
2025-02-10 17:52:02 +0100

    upd

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,19 +1,33 @@
-import { MagnifyingGlass } from 'react-loader-spinner';
+import { RingLoader } from 'react-spinners';
+// import { MagnifyingGlass } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 const Loader: React.FC = () => {
+  const override = {
+    display: 'block',
+    margin: '0 auto',
+    borderColor: 'red',
+  };
+
   return (
     <div className={css.loader}>
-      <MagnifyingGlass
-        visible={true}
-        height="150"
-        width="150"
-        ariaLabel="magnifying-glass-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        glassColor="#c0efff"
+      <RingLoader
         color="#e15b64"
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
       />
+      // <MagnifyingGlass
+      //   visible={true}
+      //   height="150"
+      //   width="150"
+      //   ariaLabel="magnifying-glass-loading"
+      //   wrapperStyle={{}}
+      //   wrapperClass=""
+      //   glassColor="#c0efff"
+      //   color="#e15b64"
+      // />
     </div>
   );
 };
