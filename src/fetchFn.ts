30f3639cea89354250967d3eaa3b562b1349d879
Uliana Seredenko <uliana1331@gmail.com>
2025-02-11 21:04:48 +0100

    fetch ts

--- src/fetchFn.ts
+++ src/fetchFn.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const ACCESS_KEY = 'Vldx9OWNWrKmNyDqVJSzU58w3g1RnpwGkRl2-FTRFP8';
 
-async function fetchPhotos(searchQuery, page) {
+async function fetchPhotos(searchQuery: string, page: number) {
   const url = `https://api.unsplash.com/search/photos?client_id=${ACCESS_KEY}&query=${searchQuery}&page=${page}&per_page=15`;
 
   const { data } = await axios.get(url);
